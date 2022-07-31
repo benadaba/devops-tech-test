85982af635507d4b696f00222188cdb2e80633c4
benadaba <adabankah.com@gmail.com>
2022-07-31 17:08:28 +0100

    add function to create add seed data to the database

--- submissionscript/db-upgrade.py
+++ submissionscript/db-upgrade.py
@@ -17,3 +17,20 @@ db_connection = mysql.connector.connect(
   passwd=PASSWD,
   database=DATABASE
 )
+
+
+def execute_seed_version_data():
+  """
+  Function to input seed data into sql database
+  """
+  #pick seed script from the relevant folder
+  current_file = Path(f'../{DBSCRIPTS}/seed_data/seeddata.sql')
+  # print(current_file)
+  with open(current_file, 'r') as f:
+      with db_connection.cursor() as cursor:
+        #set multi to True to read multiple sql statemetns from the script
+          cursor.execute(f.read(), multi=True)
+      db_connection.commit()
+
+#run function
+execute_seed_version_data()
